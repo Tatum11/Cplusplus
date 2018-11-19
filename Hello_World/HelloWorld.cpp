92157e19af3317f15c9295df0ddfe50d650589a0
Brandon Tatum <btatum3pvamu@gmail.com>
2018-11-18 22:10:06 -0600

    Update HelloWorld.cpp

--- Hello_World/HelloWorld.cpp
+++ Hello_World/HelloWorld.cpp
@@ -2,11 +2,14 @@
 #include <string>	
 using namespace std;
 
-
+string fName;
 
 int main()
 {
-	cout << "Hello World";
+	cout << "Please Enter Name" << endl;
+	cin >> fName;
+	cout << "Hello World" << endl;
+	cout << "Hi" + fName << endl; 
 	system("pause");
 
 }
